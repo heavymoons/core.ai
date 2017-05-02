2253edfee57a055d001bd282eda597029b0c208a
Naoto Ishikawa <ishinao@ishinao.net>
2017-05-02 09:49:13 +0900

    ノードに不要なイベントを削除

--- heavymoons.core.AI/BaseNode.cs
+++ heavymoons.core.AI/BaseNode.cs
@@ -8,27 +8,9 @@ namespace heavymoons.core.AI
 
         public BlackBoard BlackBoard { get; } = new BlackBoard();
 
-        public NodeEvent OnRegisterEvent;
         public NodeEvent OnExecuteEvent;
-        public NodeEvent OnExitEvent;
-        public NodeEvent OnEnterEvent;
 
         public abstract bool Execute(IMachine machine, IState state);
-
-        public void OnRegister(IMachine machine, IState state, INode node)
-        {
-            OnRegisterEvent?.Invoke(machine, state, node);
-        }
-
-        public void OnExit(IMachine machine, IState state, INode node)
-        {
-            OnExitEvent?.Invoke(machine, state, node);
-        }
-
-        public void OnEnter(IMachine machine, IState state, INode node)
-        {
-            OnEnterEvent?.Invoke(machine, state, node);
-        }
 
         public void OnExecute(IMachine machine, IState state, INode node)
         {
--- heavymoons.core.AI/Interfaces/INode.cs
+++ heavymoons.core.AI/Interfaces/INode.cs
@@ -6,8 +6,5 @@
         string Name { get; }
         bool Execute(IMachine machine, IState state);
         void OnExecute(IMachine machine, IState state, INode node);
-        void OnRegister(IMachine machine, IState state, INode node);
-        void OnExit(IMachine machine, IState state, INode node);
-        void OnEnter(IMachine machine, IState state, INode node);
     }
 }
